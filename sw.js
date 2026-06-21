35359451a6ca30c849736e7897213fcda3805b41
orsservice20-crypto <orsservice20@gmail.com>
2026-06-21 21:47:15 +0100

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
 // EniBusiness Pro SW — mise à jour automatique dès que le code change sur GitHub
-const VERSION = "enibusiness-2026-06-21-v5"; // Change automatiquement à chaque déploiement GitHub
+const VERSION = "enibusiness-2026-06-21-v6"; // Change automatiquement à chaque déploiement GitHub
 
 self.addEventListener("install", e => {
   // S'activer immédiatement sans attendre que les anciens onglets se ferment
